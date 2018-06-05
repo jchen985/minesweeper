3e69a1d6cd2aecaa40b30d87116ec3e7791f6468
jchen985 <jchen985@uwo.ca>
2018-06-05 08:19:49 -0400

    first commit

---
+++ src/minesweeper.js
@@ -0,0 +1,12 @@
+const blankline = '   |   |   ';
+console.log('This is the empty one:');
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
+
+const guessline = ' 1 |   |   ';
+const bombline = '   | B |   ';
+console.log('This is what a board with a guess line look like: ');
+console.log(guessline);
+console.log(bombline);
+console.log(blankline);
