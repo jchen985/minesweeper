6b48ec8bff4a1dd401bd3429166c8130127c0cd1
septdays06 <septdays06@septday04deMini.local>
2018-06-06 12:43:51 -0400

    stage 2

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,32 @@
-const blankline = '   |   |   ';
-console.log('This is the empty one:');
-console.log(blankline);
-console.log(blankline);
-console.log(blankline);
+// const blankline = '   |   |   ';
+// console.log('This is the empty one:');
+// console.log(blankline);
+// console.log(blankline);
+// console.log(blankline);
+//
+// const guessline = ' 1 |   |   ';
+// const bombline = '   | B |   ';
+// console.log('This is what a board with a guess line look like: ');
+// console.log(guessline);
+// console.log(bombline);
+// console.log(blankline);
+const printBoard = (board) =>{
+  console.log('Current Board: \n');
+  let i = 0;
+  while (i < 3){
+    console.log(board[i].join(' | '));
+    i ++;
+  }
+}
 
-const guessline = ' 1 |   |   ';
-const bombline = '   | B |   ';
-console.log('This is what a board with a guess line look like: ');
-console.log(guessline);
-console.log(bombline);
-console.log(blankline);
+let board = [];
+board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
